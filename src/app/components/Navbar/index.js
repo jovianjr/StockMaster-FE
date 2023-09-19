a287289dd029762e525d00d69ce61ac79485508d
jovian <alphonsusjovian@gmail.com>
2023-09-19 14:25:44 +0700

    fix(navbar): z index and color

--- src/app/components/Navbar/index.js
+++ src/app/components/Navbar/index.js
@@ -47,11 +47,11 @@ export default function Navbar() {
 					/>
 				</div>
 				<Menu.Button className="absolute right-4 top-1/2 -translate-y-1/2">
-					<Bars3Icon className="h-8 w-8" />
+					<Bars3Icon className="h-8 w-8 !p-0" />
 				</Menu.Button>
 			</div>
-			<Menu.Items className="fixed left-0 top-0 h-full w-full px-10 py-12 ">
-				<div className="absolute inset-0 bg-white/20 text-black shadow-[inset_0_2px_30px_0_rgba(255,255,255,0.75)] backdrop-blur"></div>
+			<Menu.Items className="fixed left-0 top-0 z-[1000] h-full w-full px-10 py-12 ">
+				<div className="absolute inset-0 bg-white/20 text-black shadow-[inset_0_2px_30px_0_rgba(255,255,255,0.5)] backdrop-blur"></div>
 				<div className="relative z-10 flex h-full flex-col items-end gap-3">
 					<Menu.Item className="mb-10">
 						{({ close }) => (
@@ -67,7 +67,7 @@ export default function Navbar() {
 					<CustomMenu text="Updates" href="/updates" />
 					<div className="grow"></div>
 					<button
-						className="w-full rounded-lg bg-red-500/20 py-4 text-center text-red-500"
+						className="w-full rounded-lg bg-red-500/30 py-4 text-center font-semibold text-red-500 backdrop-blur"
 						onClick={logout}
 					>
 						Logout
@@ -84,8 +84,8 @@ const CustomMenu = ({ text = '', href = '', active = false }) => {
 			<Link
 				href={href}
 				className={clsx(
-					'w-full rounded-lg py-4 text-center',
-					active ? 'bg-purple-500 font-bold' : 'bg-white/20'
+					'w-full rounded-lg py-4 text-center font-semibold backdrop-blur',
+					active ? 'bg-purple-500 font-bold' : 'bg-white/30'
 				)}
 			>
 				{text}
