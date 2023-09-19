48416683cc2822d1c6f83392a060148a82e7a81f
jovian <alphonsusjovian@gmail.com>
2023-09-19 13:32:30 +0700

    feat(profil): add tantangan, learning, updates section

---
+++ src/app/components/Box/index.js
@@ -0,0 +1,17 @@
+import clsx from 'clsx';
+
+const Box = ({ children = null, className = '' }) => {
+	return (
+		<div
+			className={clsx(
+				'h-full w-full rounded-xl p-6',
+				'bg-white/10 shadow-[inset_2px_2px_10px_0_rgba(255,255,255,0.15)] backdrop-blur-lg',
+				className
+			)}
+		>
+			{children}
+		</div>
+	);
+};
+
+export default Box;
