e992689d0620127571d54d125b28507abf47d2ae
Alphonsus Jovian Joy Reynaldo <55451337+jovianjr@users.noreply.github.com>
2023-09-22 13:34:58 +0700

    feat(learning): add list learn and detail page (#6)

---
+++ src/app/learning/[slug]/page.js
@@ -0,0 +1,44 @@
+'use client';
+import Image from 'next/image';
+import { useParams } from 'next/navigation';
+
+import Navbar from '@/app/components/Navbar';
+import Button from '@/app/components/Button';
+import Box from '@/app/components/Box';
+
+export default function Updates() {
+	const params = useParams();
+
+	return (
+		<>
+			<Navbar backTo="/learning" />
+			<main className="flex min-h-screen flex-col items-center justify-center gap-5 px-10 pb-10 pt-24">
+				<div className="flex flex-col items-center px-5 py-4">
+					<h2 className="text-2xl font-semibold">Stock Updates</h2>
+				</div>
+				<Box className="p-3">
+					<div className="relative aspect-[16/9] w-full">
+						<Image
+							src="/assets/images/placeholder/pattern.png"
+							alt=""
+							className="rounded-lg object-cover"
+							fill
+						/>
+					</div>
+				</Box>
+				<p className="text-justify text-sm font-light">
+					Pola Bearish Flag terjadi setelah tren naik yang kuat. Pola ini terlihat seperti
+					bendera dengan tiang yang menunjukkan tren naik sebelumnya dan koreksi harga
+					yang relatif pendek yang bergerak secara horizontal atau sedikit turun.
+					Bentuknya seperti segitiga yang menurun atau lurus dengan garis miring ke bawah.
+					Pola Bearish Flag adalah sinyal bahwa tren naik mungkin akan melanjutkan
+					pergerakannya setelah koreksi harga singkat. Ini mengindikasikan bahwa penjual
+					masih dominan meskipun terjadi koreksi harga. Trader sering mencari peluang sell
+					saat harga keluar dari pola ini ke bawah.
+				</p>
+				<div className="grow"></div>
+				<Button href={`/learning/${params.slug}/quiz`} text="Selanjutnya" />
+			</main>
+		</>
+	);
+}
---
+++ src/app/learning/page.js
@@ -0,0 +1,80 @@
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import Navbar from '@/app/components/Navbar';
+import Box from '@/app/components/Box';
+import clsx from 'clsx';
+
+export default function Learning() {
+	return (
+		<>
+			<Navbar />
+			<main className="px-6 pb-10 pt-24">
+				<div className="flex flex-col gap-4 py-4">
+					<h2 className="text-2xl font-semibold">List Pola Saham</h2>
+					<div className="flex flex-col gap-3 px-2">
+						{[
+							{ score: null },
+							{ score: 80 },
+							{ score: 100 },
+							{ score: 0 },
+							{ score: 80 },
+							{ score: 0 }
+						].map(val => {
+							return (
+								<Link
+									href="/learning/1"
+									className="group flex items-center gap-4"
+									key={new Date()}
+								>
+									<Box className="relative flex gap-4 overflow-hidden">
+										{val.score > 0 ? (
+											<>
+												<div
+													className={clsx(
+														'absolute left-0 top-0 z-0 h-full w-full',
+														val.score == 100 ? 'bg-green-500/20' : '',
+														val.score < 100 && val.score > 0
+															? 'bg-yellow-500/10'
+															: ''
+													)}
+												></div>
+												<span
+													className={clsx(
+														'absolute right-3 top-3 rounded-full px-2 py-0.5 text-[0.625rem]',
+														val.score == 100 ? 'bg-green-500' : '',
+														val.score < 100 && val.score > 0
+															? 'bg-yellow-500'
+															: ''
+													)}
+												>
+													{val.score ?? 0}%
+												</span>
+											</>
+										) : null}
+										<div className="relative z-10 aspect-square w-1/3">
+											<Image
+												src="/assets/images/placeholder/pattern.png"
+												alt=""
+												className="rounded-lg object-cover"
+												fill
+											/>
+										</div>
+										<div className="relative z-10 flex w-2/3 flex-col gap-2">
+											<h2 className="text-xl font-semibold">Bearish Flag</h2>
+											<p className="line-clamp-3 text-xs">
+												Pola Bearish Flag terjadi setelah tren naik yang
+												kuat. Pola ini terlihat seperti bendera dengan tiang
+											</p>
+										</div>
+									</Box>
+								</Link>
+							);
+						})}
+					</div>
+				</div>
+			</main>
+		</>
+	);
+}
