2cda46c134cec5a1ace0c1a13507a09e5894429e
jovian <alphonsusjovian@gmail.com>
2023-09-24 17:34:26 +0700

    feat(next-config): add rewrites url

--- next.config.js
+++ next.config.js
@@ -2,9 +2,20 @@
 const nextConfig = {
 	env: {
 		APP_ORIGIN: process.env.APP_ORIGIN,
-		API_URL: process.env.API_URL,
 		AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
 		AUTH0_DOMAIN: process.env.AUTH0_DOMAIN
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/auth/:path*',
+				destination: '/api/auth/:path*'
+			},
+			{
+				source: '/api/:path*',
+				destination: `${process.env.API_URL}:path*`
+			}
+		];
 	}
 };
 
